61b7f87816e162ed42da8eb1dd8cfac0ccb52257
John Titor <fenrir@localhost.localdomain>
2023-09-01 01:32:53 -0400

    feature: Adds error validation

--- runner.sh
+++ runner.sh
@@ -3,8 +3,8 @@
 
 FNAME=$(echo $RANDOM | sha256sum | head -c 16; echo).php
 VALID_EXT="pdf"
-PAYLOAD='payloads/payload_example1.php'
-#PAYLOAD='payloads/payload_example2.php'
+#PAYLOAD='payloads/payload_example1.php'
+PAYLOAD='payloads/payload_example2.php'
 OUTPUT_FNAME='b4d.zip'
 
 # LAUNCH B4Dzip
@@ -14,8 +14,11 @@ python3 main.py \
     --payload $PAYLOAD \
     --output $OUTPUT_FNAME
 
-
-printf "\n\n*********** SUMMARY ***********\n\n"
-unzip -l $OUTPUT_FNAME
-printf "\n\n"
-hexdump -C  $OUTPUT_FNAME
+if [ -e $OUTPUT ]; then
+    printf "\n\n*********** SUMMARY ***********\n\n"
+    unzip -l $OUTPUT_FNAME
+    printf "\n\n"
+    hexdump -C  $OUTPUT_FNAME
+else
+    echo "[ERROR] The zip file has not been generated due to errors."
+fi
